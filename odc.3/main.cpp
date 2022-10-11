448c366ce10547f3a98c6841182ea3b51abf7866
jakob-a2k <jakub.a2k@gmail.com>
2022-10-11 20:23:30 +0200

    constructors and destructors

---
+++ odc.3/main.cpp
@@ -0,0 +1,14 @@
+        //Program przechowujacy wydarzenia w pamieci komputera
+#include <iostream>
+#include "event.h"
+
+using namespace std;
+
+int main()
+{
+    Event w1("Apokalipsa Zombie");
+
+    w1.show();
+
+    return 0;
+}
